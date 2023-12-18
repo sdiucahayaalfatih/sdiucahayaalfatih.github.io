509446bbae8dca856978810d4382ecb78bc4ff40
Admin SDIU <sdiucahayaalfatih@gmail.com>
2023-12-18 09:19:55 +0700

    update 1.2.8

--- json/modalscript.js
+++ json/modalscript.js
@@ -1,34 +1,44 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+const modalWrapper = document.querySelector(".bacground-wrapper");
+const carousel = document.querySelector(".slide-image");
+const images = document.querySelectorAll(".slide-image img");
+const prevButton = document.getElementById("prev");
+const nextButton = document.getElementById("next");
+const closeButton = document.getElementById("close");
 
-function minSlides(n) {
-  showSlides((slideIndex += n));
-}
+let imageIndex = 0;
+let intervalId;
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("modalContainer").style.display = "none";
-}
-
-// Tampilkan modal saat halaman dimuat
-window.onload = function () {
-  document.getElementById("modalContainer").style.display = "block";
+const autoSlide = () => {
+  intervalId = setInterval(() => slideImage(true), 6000);
 };
+
+const slideImage = (next) => {
+  if (next) {
+    imageIndex = (imageIndex + 1) % images.length;
+  } else {
+    imageIndex = (imageIndex - 1 + images.length) % images.length;
+  }
+  carousel.style.transform = `translateX(-${imageIndex * 100}%)`;
+};
+
+const updateClick = (e) => {
+  clearInterval(intervalId);
+  if (e.target.id === "next") {
+    slideImage(true);
+  } else {
+    slideImage(false);
+  }
+  autoSlide();
+};
+
+prevButton.addEventListener("click", updateClick);
+nextButton.addEventListener("click", updateClick);
+
+modalWrapper.addEventListener("mouseover", () => clearInterval(intervalId));
+modalWrapper.addEventListener("mouseleave", autoSlide);
+
+closeButton.addEventListener("click", () => {
+  modalWrapper.style.display = "none";
+});
+
+autoSlide();
